5e19efc40519027c77bb4d8b4e37a11386088a3a
YIMEng-0 <1546946428@qq.com>
2021-11-28 15:48:42 +0800

    20211128

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ from jinja2 import Template
 import configparser
 
 """
+bottle 轻量级框架的简单学习，这个框架充当 Web 服务器，协调 代码与前端之间的交互
 一个bottle程序分为四部分
 1.import bottle
 2.@route('/index')定义一个访问路径，这样从浏览器访问时就能访问自己定义的路径
@@ -24,21 +25,24 @@ def hello():
     
 run(host='localhost', port=8080)
 """
+# 设置B/S 架构下面的网络应用的开始页面，进去之后直接显示光谱仪定标的界面
 @route('/')
 def index():
     return template('templates/光谱仪定标.html')
 
+# 这个是和前端的上传文件控件进行的绑定，可以设置文件保存的路径
 @route('/1', method='POST')
 def backData():
     upload = request.files.get('Spectral_Data')
     upload.save(r'./Spectral_Data')
     return 'ok'
 
+# 这个是和前端的按钮进行的绑定，点击按钮调用后端的 main.py 的脚本文件，进行图片的生成
 @route('/getCalibration', method='POST')
 def getCalibration():
     os.system("python main.py")
 
-# 进行图片文件的上传
+# 进行图片文件的上传（目前存在问题，导致上传文件是失败的）
 route("/image", methods = 'GET')
 def sendImage():
     skuid = request.args.get('skuid')
