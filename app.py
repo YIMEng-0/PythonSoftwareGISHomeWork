423e0d178ef5acfdabda15496b4221819e6aa5b0
YIMEng-0 <1546946428@qq.com>
2021-11-28 15:12:25 +0800

    20211128

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
+import base64
+
 from bottle import route, run, static_file, template, request
 from bottle import jinja2_view as view, jinja2_template as template
 import os
+
+from flask import make_response
 
 from jinja2 import Template
 import configparser
@@ -11,7 +15,6 @@ import configparser
 2.@route('/index')定义一个访问路径，这样从浏览器访问时就能访问自己定义的路径
 3.定义一个与第2步route相应的处理函数，系统在看到@route标记后会自动调用其后边的这个函数。
 4.调用run()函数。
-
 
 from bottle import route, run
 
@@ -35,5 +38,16 @@ def backData():
 def getCalibration():
     os.system("python main.py")
 
+# 进行图片文件的上传
+route("/image", methods = 'GET')
+def sendImage():
+    skuid = request.args.get('skuid')
+    print("1231231233")
+    img_local_path = "Middle Light Calibration Difference(Numpy - Myself).png"
+    img_f = open(img_local_path, 'rb')
+    res = make_response(img_f.read())   # 用flask提供的make_response 方法来自定义自己的response对象
+    res.headers['Content-Type'] = 'image/png'   # 设置response对象的请求头属性'Content-Type'为图片格式
+    img_f.close()
+    return res
 
 run(host='localhost', port=8088, reloader=True, debug=True)
